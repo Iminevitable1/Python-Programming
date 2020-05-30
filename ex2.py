4261894cb631e119bf4f6dd97b2fc8c1f2492608
Iminevitable1 <65622445+Iminevitable1@users.noreply.github.com>
2020-05-30 13:32:33 +0530

    Add files via upload

---
+++ ex2.py
@@ -0,0 +1,5 @@
+#Comment is used so you can read your program later without any queries.
+#print("Say your name")
+print("The above line will not run. ")
+#print("Did you get it?")
+print("Yep ! I got it...")
